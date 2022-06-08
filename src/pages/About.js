444e8490d6142addbbb0afa369f9eda80258a2b3
Jessica-Ike <90731613+Jessica-Ike@users.noreply.github.com>
2022-06-08 19:57:17 -0400

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,8 +3,24 @@ import React, { Component } from 'react';
 export default class About extends Component {
   render() {
     return (
-      <div>
-        <p>Design your About me page </p> 
+       <div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+          // Image goes here
+              alt="Profile Pic"
+             ></img>
+          </div>
+      </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Jessica Ike</div>
+              <div className="brief_description">
+              My name is Jessica Ike, I am a rising juinor at the University of Houston studying Computer Science.
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
