4a1c3733f7e86aa50a92048cf0c46c977f21caef
jike <jessicaike123@gmail.com>
2022-06-12 18:49:52 -0400

    Enter

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,41 +1,30 @@
 import React, { Component } from 'react';
-import { Button, TextField } from "@mui/material";
+import "./About.css";
+import profile_pic_name from "../assets/profile_pic_name.jpg";
   
 export default class About extends Component {
   render() {
     return (
-      <TextField
-        label="Add New Item"
-        variant="outlined"
-        //onChange={call handleChange here}
-        //value={set the content state here}
-      />
-    <Button
-      style={{ marginLeft: "10px" }}
-      //onClick={call handleSubmit here}
-      variant="contained"
-      color="primary"
-    >
-    Add
-    </Button>
-
-       <div>
-        <div class="split left">
-          <div className="centered">
-            <img 
-              className="profile_image"
-                src=""
-              alt="Profile Pic"
-             >
-          </div>
-      </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Jessica Ike</div>
-              <div className="brief_description">
-              My name is Jessica Ike, I am a rising juinor at the University of Houston studying Computer Science.
+      <div>
+        <div>
+          <div class="split left">
+            <div className="centered">
+              <img 
+                className="profile_image"
+                // Image goes here
+                src = {profile_pic_name}
+                alt="Profile Pic"
+              ></img>
             </div>
           </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Jessica Ike</div>
+                <div className="brief_description">
+                  Hello! My name is Jessica Ike, I am a Junior at the University of Houston studying Computer Science 
+                </div>
+              </div>
+            </div>
         </div>
       </div>
     )
